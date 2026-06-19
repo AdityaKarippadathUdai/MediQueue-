c04d6215b086f47b150ce28efd8e9d2d149ee300
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-19 09:48:11 +0530

    updated patient controller

--- server/src/controllers/patientController.ts
+++ server/src/controllers/patientController.ts
@@ -5,45 +5,45 @@ export class PatientController {
   async getPatients(req: Request, res: Response, next: NextFunction) {
     try {
       const patients = await patientService.getPatients();
-      return res.status(200).json(patients);
-    } catch (error) {
-      next(error);
+      res.status(200).json(patients);
+    } catch (err) {
+      next(err);
     }
   }
 
   async getPatientById(req: Request, res: Response, next: NextFunction) {
     try {
       const patient = await patientService.getPatientById(req.params.id);
-      return res.status(200).json(patient);
-    } catch (error) {
-      next(error);
+      res.status(200).json(patient);
+    } catch (err) {
+      next(err);
     }
   }
 
   async addPatient(req: Request, res: Response, next: NextFunction) {
     try {
       const patient = await patientService.addPatient(req.body);
-      return res.status(201).json(patient);
-    } catch (error) {
-      next(error);
+      res.status(201).json(patient);
+    } catch (err) {
+      next(err);
     }
   }
 
   async updatePatientStatus(req: Request, res: Response, next: NextFunction) {
     try {
       const patient = await patientService.updatePatientStatus(req.params.id, req.body);
-      return res.status(200).json(patient);
-    } catch (error) {
-      next(error);
+      res.status(200).json(patient);
+    } catch (err) {
+      next(err);
     }
   }
 
   async deletePatient(req: Request, res: Response, next: NextFunction) {
     try {
       await patientService.deletePatient(req.params.id);
-      return res.status(200).json({ message: 'Patient removed successfully' });
-    } catch (error) {
-      next(error);
+      res.status(200).json({ message: 'Patient removed from queue successfully.' });
+    } catch (err) {
+      next(err);
     }
   }
 }
