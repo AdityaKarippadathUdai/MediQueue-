264f12d30f8c7e80066a2ce839a89eb695d4fe38
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-20 19:21:14 +0530

    refactor patient controller  to use queue service and patient priority

--- server/src/controllers/patientController.ts
+++ server/src/controllers/patientController.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import patientService from '../services/patientService';
+import queueService from '../services/queueService';
+import { PatientPriority } from '../models/Patient';
 
 export class PatientController {
   async getPatients(req: Request, res: Response, next: NextFunction) {
     try {
-      const patients = await patientService.getPatients();
+      const patients = await queueService.getPatients();
       res.status(200).json(patients);
     } catch (err) {
       next(err);
@@ -13,7 +14,7 @@ export class PatientController {
 
   async getPatientById(req: Request, res: Response, next: NextFunction) {
     try {
-      const patient = await patientService.getPatientById(req.params.id);
+      const patient = await queueService.getPatientById(req.params.id);
       res.status(200).json(patient);
     } catch (err) {
       next(err);
@@ -22,7 +23,7 @@ export class PatientController {
 
   async addPatient(req: Request, res: Response, next: NextFunction) {
     try {
-      const patient = await patientService.addPatient(req.body);
+      const patient = await queueService.addPatient(req.body);
       res.status(201).json(patient);
     } catch (err) {
       next(err);
@@ -31,7 +32,7 @@ export class PatientController {
 
   async updatePatientStatus(req: Request, res: Response, next: NextFunction) {
     try {
-      const patient = await patientService.updatePatientStatus(req.params.id, req.body);
+      const patient = await queueService.updatePatientStatus(req.params.id, req.body);
       res.status(200).json(patient);
     } catch (err) {
       next(err);
@@ -40,8 +41,17 @@ export class PatientController {
 
   async deletePatient(req: Request, res: Response, next: NextFunction) {
     try {
-      await patientService.deletePatient(req.params.id);
+      await queueService.deletePatient(req.params.id);
       res.status(200).json({ message: 'Patient removed from queue successfully.' });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getPatientStatus(req: Request, res: Response, next: NextFunction) {
+    try {
+      const statusInfo = await queueService.getPatientStatus(req.params.id);
+      res.status(200).json(statusInfo);
     } catch (err) {
       next(err);
     }
