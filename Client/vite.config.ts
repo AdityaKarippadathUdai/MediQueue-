a318d2fd9360b7eda4dba8c16bdcd8308c47ce82
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-22 14:03:29 +0530

    feat:Add vite PWA

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -2,10 +2,29 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'sw.ts',
+        injectRegister: false,
+        manifest: false,
+        includeAssets: ['pwa-icon.svg', 'pwa-icon-192.png', 'pwa-icon-512.png', 'manifest.webmanifest'],
+        injectManifest: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+        },
+        devOptions: {
+          enabled: true,
+          type: 'module',
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
