3c967bfa8446818b7c21c133b899e611bc246f97
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-28 23:35:54 +0530

    updated cor config

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,6 @@ app.use(
     origin: config.corsOrigin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
