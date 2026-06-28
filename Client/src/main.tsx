16484f465acab178fd1a36d805777d96f0d93e93
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-28 21:39:44 +0530

    refactor to remove unwanted imports

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,9 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import {registerServiceWorker} from './pwa/registerServiceWorker';
-
-registerServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
