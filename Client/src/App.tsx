6722f5afcd4c267991788654d2cb4a5be8d726b2
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-28 21:39:25 +0530

    feat:Add pwa update component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -6,11 +6,13 @@
 import React from 'react';
 import { QueueProvider } from './context/QueueContext';
 import { AppRouter } from './router/AppRouter';
+import { PWAUpdatePrompt } from './components/PWAUpdatePrompt';
 
 export default function App() {
   return (
     <QueueProvider>
       <AppRouter />
+      <PWAUpdatePrompt />
     </QueueProvider>
   );
 }
